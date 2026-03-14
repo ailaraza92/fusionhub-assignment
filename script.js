db2d4f948d520bf9ddd1a1e5413ac5c5c061de09
syeda aila raza <officialfactologyy@gmail.com>
2026-03-14 06:09:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,6 @@
-// MOBILE MENU
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
 
-const menu = document.getElementById("menu-toggle");
-const nav = document.getElementById("nav-links");
-
-menu.addEventListener("click", () => {
-
-nav.classList.toggle("active");
-
+menuToggle.addEventListener("click", () => {
+navLinks.classList.toggle("active");
 });
-
-
-// SCROLL TO TOP
-
-const topBtn = document.getElementById("topBtn");
-
-window.onscroll = () => {
-
-if(document.documentElement.scrollTop > 200){
-
-topBtn.style.display = "block";
-
-}else{
-
-topBtn.style.display = "none";
-
-}
-
-};
-
-topBtn.onclick = () => {
-
-window.scrollTo({
-top:0,
-behavior:"smooth"
-});
-
-};
